d5831616473775478f2afb1e1057378dd206bec0
Kushagra Saxena <kushagrasaxena061@gmail.com>
2022-09-22 19:37:36 +0530

    auth-complete

---
+++ client/.history/src/App_20220922183808.js
@@ -0,0 +1,16 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Singup";
+import Login from "./components/Login";
+
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
